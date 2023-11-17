50ca09a088647a7aef0ba630df8c0bf10015a88a
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-11-17 16:19:55 +0100

    Complete Milestone 1
    Add api.php and ruote
    public function whit json file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('projects', function () {
+    return response()->json([
+        'status' => 'success',
+        'result' => App\Models\Project::all(),
+    ]);
+});
+
+Route::get('projects', [ProjectController::class, 'index']);
